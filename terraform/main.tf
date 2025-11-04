a0f8a2f4fd7efa269770fb163d0b87c846b0a699
KDS4wexp <KDS4wexp@gmail.com>
2025-11-04 03:53:48 +0300

    fix: ingress 0.0.0.0/0 ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,7 +136,7 @@ resource "yandex_vpc_security_group" "bastion_security"{
     description = "SSH"
     protocol = "TCP"
     port = 22
-    v4_cidr_blocks = ["10.0.0.10/32", "10.0.1.0/24", "85.172.104.207/32"]
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "DNS TCP"
