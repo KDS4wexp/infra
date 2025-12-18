138cf65d38bb385af3796468050a55626f7cdab9
KDS4wexp <KDS4wexp@gmail.com>
2025-12-18 17:40:54 +0300

    fix:

---
+++ terraform/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    endpoints                   = { s3 = "https://storage.yandexcloud.net" }
+    region                      = "ru-central1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    skip_metadata_api_check     = true
+  } 
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,15 +7,6 @@ terraform {
       version       = "0.119.0"
     }
   }
-  backend "s3" {
-    endpoints                   = { s3 = "https://storage.yandexcloud.net" }
-    region                      = "ru-central1"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    skip_metadata_api_check     = true
-  } 
 }
 
 provider "yandex" {
